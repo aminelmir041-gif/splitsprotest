a758a0d653d2571b3df49bb60970040aaa65aa5b
aminelmir041-gif <aminelmir041@gmail.com>
2026-09-14 00:45:13 +1000

    Attach ad attribution to quote submissions

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,11 +1,13 @@
 import axios from "axios";
+import { getAttribution } from "./attribution";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const API = `${BACKEND_URL}/api`;
 
 export const api = axios.create({ baseURL: API });
 
-export const submitQuote = (data) => api.post("/quotes", data).then((r) => r.data);
+export const submitQuote = (data) =>
+  api.post("/quotes", { ...data, ...getAttribution() }).then((r) => r.data);
 export const getReviews = () => api.get("/reviews").then((r) => r.data);
 export const uploadPhoto = (file) => {
   const fd = new FormData();
